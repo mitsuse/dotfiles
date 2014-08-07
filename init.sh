fb7b24a7dc63f6a483874b3fccc05c21d1d9cb12
Tomoya Kose <tomoya@mitsuse.jp>
2014-08-07 20:11:46 +0900

    Set $HOME to $work_dir if it is empty.

--- init.sh
+++ init.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 work_dir=$1
+localenv_dir=$(dirname $0) 
 
 if [ $# -gt 1 ]; then
     echo "$(basename $0): Too mary arguments"
     exit 1
+fi
+
+if [ -z "$work_dir" ]; then
+    work_dir=$HOME
 fi
 
 if [ ! -d "$work_dir" ]; then
