cd0bf6763abfeca244af070cc2290e774a070b66
Tomoya Kose <tomoya@mitsuse.jp>
2015-04-15 10:00:09 +0900

    Brew my favarite tools with an installation script.

---
+++ packages/brew-all.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## taps
+brew tap mitsuse/bullet
+
+## Installation
+brew install tmux zsh git
+brew install luajit
+brew install go --with-cc-common
+brew install scala typesafe-activator
+brew install boot2docker
+brew install caskroom/cask/brew-cask
+brew install bullet-cmd
+
+## Installation (Cask)
+brew cask install virtualbox
