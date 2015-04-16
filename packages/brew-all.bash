002aa28d8a11598a691f6f52980f2c209983edd0
Tomoya Kose <tomoya@mitsuse.jp>
2015-04-16 14:30:18 +0900

    Install "libyaml".

--- packages/brew-all.bash
+++ packages/brew-all.bash
@@ -10,6 +10,7 @@ brew install go --with-cc-common
 brew install scala typesafe-activator
 brew install boot2docker
 brew install leveldb
+brew install libyaml
 brew install heroku-toolbelt
 brew install caskroom/cask/brew-cask
 brew install bullet-cmd
