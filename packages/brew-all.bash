6fda64ac907e7cec7669dd86cb69d54bd6ae0e16
Tomoya Kose <tomoya@mitsuse.jp>
2015-04-16 13:15:03 +0900

    Install "leveldb".

--- packages/brew-all.bash
+++ packages/brew-all.bash
@@ -9,6 +9,7 @@ brew install luajit
 brew install go --with-cc-common
 brew install scala typesafe-activator
 brew install boot2docker
+brew install leveldb
 brew install heroku-toolbelt
 brew install caskroom/cask/brew-cask
 brew install bullet-cmd
