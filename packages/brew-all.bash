5f138e4797e0e1118a9effa929fb2dec9a93c4eb
Tomoya Kose <tomoya@mitsuse.jp>
2015-12-08 11:12:49 +0900

    Split "packages/brew-all.bash" into two files:
    One for "brew install" and the other for "brew cask install".

--- packages/brew-all.bash
+++ packages/brew-all.bash
@@ -36,14 +36,3 @@ brew install ghq
 brew install colordiff
 brew install android-sdk android-platform-tools
 brew install figlet
-
-## Installation (Cask)
-brew cask install firefox
-brew cask install google-chrome
-brew cask install java
-brew cask install goofy
-brew cask install gimp
-brew cask install atom
-brew cask install music-manager
-brew cask install appcleaner
-brew cask install clamxav
---
+++ packages/cask-all.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+brew cask install firefox
+brew cask install google-chrome
+brew cask install java
+brew cask install goofy
+brew cask install gimp
+brew cask install atom
+brew cask install music-manager
+brew cask install appcleaner
+brew cask install clamxav
